2b14fb8bc3b67126d20f8c83a7c777f0e5191b08
Chris <ccombs0418@gmail.com>
2024-07-15 13:57:03 -0400

    Added Sheety API functionality Added functionality so that when the user inputs the food they want to add, it will put it into a spreadsheet to keep track of it using Sheety's API.

--- calorietracker.py
+++ calorietracker.py
@@ -1,17 +1,22 @@
-import requests
+import requests, datetime
 nutritionix_endpoint = "https://trackapi.nutritionix.com"
 instant_endpoint = f"{nutritionix_endpoint}/v2/search/instant"
+sheety_endpoint = "https://api.sheety.co/570089251c12aa4caaf1dce1bba9b271/calorieTracker/sheet1"
 
 class CalorieTracker:
     
-    def __init__(self, api_key, application_id) -> None:
+    def __init__(self, api_key, application_id, sheety_auth_token) -> None:
         self.api_key = api_key
         self.application_id = application_id
-        self.headers = {
+        self.sheety_auth_token = sheety_auth_token
+        self.nutritionix_headers = {
             "x-app-id": self.application_id,
             "x-app-key": self.api_key,
         }
 
+        self.sheety_headers = {
+            "Authorization": f"Bearer {self.sheety_auth_token}"
+        }
 
 
     def get_food_items(self):
@@ -21,7 +26,8 @@ class CalorieTracker:
         self.get_food_items_parameters = {
             "query": self.user_input
         }
-        self.food_items_response = requests.get(url=instant_endpoint, params=self.get_food_items_parameters, headers=self.headers)
+        self.food_items_response = requests.get(url=instant_endpoint, params=self.get_food_items_parameters, headers=self.nutritionix_headers)
+        self.food_items_response.raise_for_status()
         self.food_data = self.food_items_response.json()
         print(self.food_data)
         if self.branded == "yes":
@@ -57,8 +63,28 @@ class CalorieTracker:
                     #Need to add error handling
                     self.removal_option = int(input("Which one do you want to remove? Please enter the number. \n"))
                     self.food_items.remove(self.food_items[self.removal_option - 1])
-                    self.check_food_correctness()
+                    #self.check_food_correctness()
         print("Awesome! Since there are no issues, I'll add these to the spreadsheet.")
+        if self.category == "branded":
+            self.food_name_tag = "brand_name_item_name"
+        else:
+            self.food_name_tag = "food_name"
+        self.today = datetime.datetime.now()
+        self.date = self.today.strftime("%m/%d/%Y")
+        for food_item in self.food_items:
+            self.sheety_parameters = {
+                
+                "sheet1": {
+                    "foodName": food_item[self.food_name_tag],
+                    "calories": food_item["nf_calories"],
+                    "amount": food_item["serving_qty"],
+                    "date": self.date, 
+                }
+            }
+            food_item_post = requests.post(url=sheety_endpoint, json=self.sheety_parameters, headers=self.sheety_headers)
+            food_item_post.raise_for_status()
+            print(food_item_post.text)
+
 
     def list_food_items(self):
         self.num = 1
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import calorietracker
 import os
 APPLICATION_ID = os.environ["NUTRITIONIX_APPLICATION_ID"]
 API_KEY = os.environ["NUTRITIONIX_API_KEY"]
+SHEETY_AUTH_TOKEN = os.environ["SHEETY_AUTHORIZATION_TOKEN"]
 
-
-calorietracker = calorietracker.CalorieTracker(api_key=API_KEY,application_id=APPLICATION_ID)
-
+calorietracker = calorietracker.CalorieTracker(api_key=API_KEY,application_id=APPLICATION_ID, sheety_auth_token=SHEETY_AUTH_TOKEN)
 calorietracker.get_food_items()
 calorietracker.check_food_correctness()
